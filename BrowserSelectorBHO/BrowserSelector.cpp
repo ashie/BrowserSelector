ce1d8dbecdf367f3bdac60117de4ebd8c5c9da7e
Takuro Ashie <ashie@clear-code.com>
2019-07-03 15:11:06 +0900

    Initial import

---
+++ BrowserSelectorBHO/BrowserSelector.cpp
@@ -0,0 +1,8 @@
+// BrowserSelector.cpp : CBrowserSelector ‚ÌŽÀ‘•
+
+#include "stdafx.h"
+#include "BrowserSelector.h"
+
+
+// CBrowserSelector
+
---
+++ BrowserSelectorBHO/dllmain.h
@@ -0,0 +1,10 @@
+// dllmain.h : モジュール クラスの宣言
+
+class CBrowserSelectorBHOModule : public ATL::CAtlDllModuleT< CBrowserSelectorBHOModule >
+{
+public :
+	DECLARE_LIBID(LIBID_BrowserSelectorBHOLib)
+	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_BROWSERSELECTORBHO, "{4FFE7D2B-2618-4867-AC9F-C7E35ED26F8C}")
+};
+
+extern class CBrowserSelectorBHOModule _AtlModule;
