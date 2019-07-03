14ba2bac53ab076f412ef628984198d825f20baa
Takuro Ashie <ashie@clear-code.com>
2019-07-03 16:05:31 +0900

    Forbit loading from explorer.exe

--- BrowserSelectorBHO/dllmain.cpp
+++ BrowserSelectorBHO/dllmain.cpp
@@ -11,5 +11,12 @@ CBrowserSelectorBHOModule _AtlModule;
 extern "C" BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
 {
 	hInstance;
+	if (dwReason == DLL_PROCESS_ATTACH) {
+		TCHAR loader[MAX_PATH];
+		GetModuleFileName(NULL, loader, MAX_PATH);
+		if (_tcsicmp(loader, _T("explorer.exe"))) {
+			return FALSE;
+		}
+	}
 	return _AtlModule.DllMain(dwReason, lpReserved); 
 }
