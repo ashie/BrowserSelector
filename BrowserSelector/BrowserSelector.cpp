5f9fbf70b27c585363a7b2c937129e9adc306655
Takuro Ashie <ashie@clear-code.com>
2019-07-24 18:13:32 +0900

    Open by existing IE process if it's available
    Signed-off-by: Takuro Ashie <ashie@clear-code.com>

--- BrowserSelector/BrowserSelector.cpp
+++ BrowserSelector/BrowserSelector.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <ShellAPI.h>
 #include <DbgHelp.h>
+#include <Ddeml.h>
 
 using namespace std;
 
@@ -47,9 +48,56 @@ static bool IsIntranetURL(const wstring &url, const vector<wstring> &urlPatterns
 	return false;
 }
 
+static HDDEDATA CALLBACK DDECallback(WORD wType, WORD wFmt, HCONV hConv, HSZ hsz1, HSZ hsz2, HDDEDATA hData, DWORD lData1, DWORD lData2)
+{
+	return (HDDEDATA)0;
+}
+
+bool OpenByExistingIE(const wstring &url)
+{
+	DWORD dwDDEID = 0;
+	UINT err;
+
+	err = DdeInitializeW(
+			&dwDDEID,
+			(PFNCALLBACK)MakeProcInstance((FARPROC)DDECallback, ghInstance),
+			CBF_SKIP_ALLNOTIFICATIONS | APPCMD_CLIENTONLY, 0L);
+
+	if (err != DMLERR_NO_ERROR)
+		return false;
+
+	HSZ hszService = DdeCreateStringHandleW(dwDDEID, L"IEXPLORE", CP_WINUNICODE);
+	HSZ hszTopic = DdeCreateStringHandleW(dwDDEID, L"WWW_OpenURL", CP_WINUNICODE);
+	HCONV hConv = DdeConnect(dwDDEID, hszService, hszTopic, NULL);
+	DdeFreeStringHandle(dwDDEID, hszService);
+	DdeFreeStringHandle(dwDDEID, hszTopic);
+
+	if(!hConv)
+		return false;
+
+	CString cmd = url.c_str();
+	HDDEDATA hDDEData = DdeClientTransaction(
+		(LPBYTE)url.c_str(),
+		static_cast<DWORD>(((url.size() + 1) * sizeof(wchar_t))),
+		hConv,
+		0,
+		0,
+		XTYP_EXECUTE,
+		10000,
+		NULL);
+	if (hDDEData)
+		DdeFreeDataHandle(hDDEData);
+	DdeDisconnect(hConv);
+	DdeUninitialize(dwDDEID);
+
+	return true;
+}
+
 void OpenByIE(const wstring &url)
 {
-	::ShellExecute(NULL, _T("open"), _T("iexplore.exe"), url.c_str(), NULL, SW_SHOW);
+	bool succeeded = OpenByExistingIE(url);
+	if (!succeeded)
+		::ShellExecute(NULL, _T("open"), _T("iexplore.exe"), url.c_str(), NULL, SW_SHOW);
 }
 
 void OpenByFirefox(const wstring &url)
