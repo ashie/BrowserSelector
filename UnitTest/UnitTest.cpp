3419f7cc4cc174088b25c89dfa69d53560a6f6c4
Takuro Ashie <ashie@clear-code.com>
2020-08-12 12:10:36 +0900

    Add some unit tests
    Signed-off-by: Takuro Ashie <ashie@clear-code.com>

---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,56 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../BrowserSelector/BrowserSelectorCommon.h"
+
+using namespace std;
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(MatchSimpleWildCard)
+	{
+	public:
+
+		TEST_METHOD(WildCard)
+		{
+			wstring url(L"https://www.example.com");
+			wstring pattern(L"https://*.example.com");
+			Assert::IsTrue(matchSimpleWildCard(url, pattern));
+		}
+
+		TEST_METHOD(UnmatchedHost)
+		{
+			wstring url(L"https://www2.example.com");
+			wstring pattern(L"https://www.example.com");
+			Assert::IsFalse(matchSimpleWildCard(url, pattern));
+		}
+
+		TEST_METHOD(CaseInsensitiveScheme)
+		{
+			wstring url(L"HTTPS://www.example.com");
+			wstring pattern(L"https://*.example.com");
+			Assert::IsTrue(matchSimpleWildCard(url, pattern));
+		}
+
+		TEST_METHOD(CaseInsensitiveHostName)
+		{
+			wstring url(L"https://WWW.example.com");
+			wstring pattern(L"https://www.example.com");
+			Assert::IsTrue(matchSimpleWildCard(url, pattern));
+		}
+
+		TEST_METHOD(CaseInsensitiveDomain)
+		{
+			wstring url(L"https://www.EXAMPLE.COM");
+			wstring pattern(L"https://www.example.com");
+			Assert::IsTrue(matchSimpleWildCard(url, pattern));
+		}
+
+		TEST_METHOD(CaseInsensitivePath)
+		{
+			wstring url(L"https://www.example.com/Path/To/RESOURCE");
+			wstring pattern(L"https://www.example.com/path/to/resource");
+			Assert::IsTrue(matchSimpleWildCard(url, pattern));
+		}
+	};
+}
